eb2fa0d69eec79f1268c668b5f37229bedf26393
Dhanush S <dhanush6102002@gmail.com>
2025-01-29 23:33:30 +0530

    PATCH: Change time format

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -3,31 +3,31 @@ import moment from 'moment';
 
 const logger = {
     info: (message: string) => {
-        console.info(`[${moment().format('YYYY-MM-DD HH:mm:ss')}]`.grey, `[INFO] `.yellow, message);
+        console.info(`[${moment().format('DD-MM-YYYY HH:mm:ss')}]`.grey, `[INFO] `.yellow, message);
     },
     warn: (message: string) => {
-        console.warn(`[${moment().format('YYYY-MM-DD HH:mm:ss')}]`.grey, `[WARN] `.magenta, message);
+        console.warn(`[${moment().format('DD-MM-YYYY HH:mm:ss')}]`.grey, `[WARN] `.magenta, message);
     },
     error: (message: string) => {
-        console.error(`[${moment().format('YYYY-MM-DD HH:mm:ss')}]`.grey, `[ERROR] `.red, message);
+        console.error(`[${moment().format('DD-MM-YYYY HH:mm:ss')}]`.grey, `[ERROR] `.red, message);
     },
     debug: (message: string) => {
-        console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}]`.grey, `[DEBUG] `.grey, message);
+        console.log(`[${moment().format('DD-MM-YYYY HH:mm:ss')}]`.grey, `[DEBUG] `.grey, message);
     },
     log: (message: string) => {
-        console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}]`.grey, `[LOG] `.grey, message);
+        console.log(`[${moment().format('DD-MM-YYYY HH:mm:ss')}]`.grey, `[LOG] `.grey, message);
     },
     event: (event: string, message: string) => {
-        console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}]`.grey,`[${event}] `.cyan, message);
+        console.log(`[${moment().format('DD-MM-YYYY HH:mm:ss')}]`.grey,`[${event}] `.cyan, message);
     },
     success: (from: string, message: string) => {
-        console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}]`.grey, `[${from}] `.green, message);
+        console.log(`[${moment().format('DD-MM-YYYY HH:mm:ss')}]`.grey, `[${from}] `.green, message);
     },
     fail: (from: string, message: string) => {
-        console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}]`.grey, `[${from}] `.red, message);
+        console.log(`[${moment().format('DD-MM-YYYY HH:mm:ss')}]`.grey, `[${from}] `.red, message);
     },
     warning: (from: string, message: string) => {
-        console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}]`.grey, `[${from}] `.yellow, message);
+        console.log(`[${moment().format('DD-MM-YYYY HH:mm:ss')}]`.grey, `[${from}] `.yellow, message);
     },
     clear: () => {
         console.clear();
