b3d07ff0a91876c476b71e9e964cd797ded8b8e7
Dhanush S <dhanush6102002@gmail.com>
2025-01-30 02:10:36 +0530

    PATCH: Emit event to Discord Client

--- src/types/client.ts
+++ src/types/client.ts
@@ -3,4 +3,5 @@ import { Collection, Client as DiscordClient, ClientUser } from "discord.js";
 export interface CustomDiscordClient extends DiscordClient {
     commands?: Collection<string, any>;
     user: ClientUser | null
+    emit(event: string | symbol, ...args: any[]): boolean;
 }
